1421aae2c18311f50cecb84672485c472ed0d79d
cdiamond3 <diamond.chris.3@gmail.com>
2021-11-16 17:29:27 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { initializeApp } from "@firebase/app";
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [downloadUrl, setDownloadUrl] = useState(null);
+
+  let firebase = initializeApp;
+
+  const handleChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    let file = image;
+    let storage = firebase.storage();
+    let storageRef = storage.ref();
+    let uploadTask = storageRef.child("folder/" + file.name).put(file);
+
+    uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) => {
+        let progress =
+          Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress({ progress });
+      },
+      (error) => {
+        throw error;
+      },
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+          setDownloadUrl({
+            downloadURL: url,
+          });
+        });
+        document.getElementById("file").value = null;
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h4>upload image</h4>
+        <label>
+          Choose file
+          <input type="file" id="file" onChange={handleChange} />
+        </label>
+
+        {progress}
+        <button className="button" onClick={handleUpload}>
+          Upload
+        </button>
+        <img
+          className="ref"
+          src={downloadUrl || "https://via.placeholder.com/400x300"}
+          alt="Uploaded Images"
+          height="300"
+          width="400"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { initializeApp } from "firebase/app";
+
+
+export default function firebaseImageUpload() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyB2CUDpQE5u_yKXRtJV0SJHrCDHMGVSWLI",
+    authDomain: "ump-signage-official.firebaseapp.com",
+    projectId: "ump-signage-official",
+    storageBucket: "ump-signage-official.appspot.com",
+    messagingSenderId: "634236241849",
+    appId: "1:634236241849:web:87274c19210a3a61a8d868",
+    measurementId: "G-8XN961N6LZ",
+  };
+
+  
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+}
